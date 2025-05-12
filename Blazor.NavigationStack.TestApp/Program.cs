881680e9f1a5ec4d25059f4fbd15090d9b5af89b
pipoon21 <pipoon22@hotmail.com>
2025-05-13 05:04:38 +0700

    Change project type to WASM

--- Blazor.NavigationStack.TestApp/Program.cs
+++ Blazor.NavigationStack.TestApp/Program.cs
@@ -1,27 +1,9 @@
 using Blazor.NavigationStack.TestApp.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-       .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-   .AddInteractiveServerRenderMode();
-
-app.Run();
+await builder.Build().RunAsync();
