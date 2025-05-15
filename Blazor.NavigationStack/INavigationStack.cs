a7da61bb08d02c6a11b9b21271162e38fe6137a3
pipoon21 <pipoon22@hotmail.com>
2025-05-16 03:33:23 +0700

    Update Push method in INavigationStack to return a boolean and add clarifying comments.

--- Blazor.NavigationStack/INavigationStack.cs
+++ Blazor.NavigationStack/INavigationStack.cs
@@ -13,11 +13,16 @@ public interface INavigationStack {
     /// Push a page onto the <see cref="NavigationStack"/>.
     /// The task will be completed when <see cref="SetResult"/> method has been invoked or the back button was clicked.
     /// </summary>
-    Task Push(StackPage stackPage);
+    /// <returns>
+    /// Return true if the current page was <see cref="Pop"/>. Return false if the current page was <see cref="Cancel"/>
+    /// or the back button was clicked.
+    /// </returns>
+    Task<bool> Push(StackPage stackPage);
 
     /// <summary>
     /// Complete the current <see cref="StackPage"/> by setting a result.
-    /// This will cause the current <see cref="StackPage"/> to be popped and return to the previous page.
+    /// This will cause the current <see cref="StackPage"/> to be popped and return to the previous page
+    /// while the <see cref="value"/> will be passed to the awaiting <see cref="Push"/> method call.
     /// </summary>
     void SetResult(object? value);
 
@@ -41,5 +46,8 @@ public interface INavigationStack {
     /// <summary>
     /// Pop the current page.
     /// </summary>
+    /// <remarks>
+    /// Equivalent to <see cref="SetResult"/> with null as parameter.
+    /// </remarks>
     void Pop();
 }
